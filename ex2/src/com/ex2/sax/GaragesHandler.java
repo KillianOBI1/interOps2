e0600fe4595423cfeacad4e46c0ac6bd08d831a5
yann <killian.monot@orange.fr>
2018-09-20 16:09:57 +0200

    SAX

---
+++ ex2/src/com/ex2/sax/GaragesHandler.java
@@ -0,0 +1,82 @@
+package com.ex2.sax;
+
+import java.io.IOException;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+public class GaragesHandler implements ContentHandler {
+	
+	public GaragesHandler() {
+		super();
+	}
+
+	@Override
+	public void characters(char[] arg0, int arg1, int arg2) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endElement(String arg0, String arg1, String arg2) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endPrefixMapping(String arg0) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ignorableWhitespace(char[] arg0, int arg1, int arg2) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processingInstruction(String arg0, String arg1) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDocumentLocator(Locator locator) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skippedEntity(String name) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startPrefixMapping(String prefix, String uri) throws SAXException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
